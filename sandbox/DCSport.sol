9144b738b14c27d636cf628c83967fc5e99e716c
Alexis <alexis@goldbaum.io>
2020-08-17 17:30:05 +0200

    Add event and sandbox files

---
+++ sandbox/DCSport.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.9; 
+
+contract DCSport {
+
+	address public creator;
+
+	address[] matches;
+
+	constructor() public {
+		
+    }
+
+    function addMatch(address match) public {
+
+    }
+}
